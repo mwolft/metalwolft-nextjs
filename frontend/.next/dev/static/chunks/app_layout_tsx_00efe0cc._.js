cf45b26576a7d94a1c2ef25de9c963cf9c195a5f
Sergio Arias Fernández <166837201+mwolft@users.noreply.github.com>
2026-04-08 06:55:43 +0000

    chore: clean generated files and fix git tracking

--- frontend/.next/dev/static/chunks/app_layout_tsx_00efe0cc._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9027c349._.css",
-  "static/chunks/node_modules__pnpm_ac9f1354._.js"
-],
-    source: "dynamic"
-});
